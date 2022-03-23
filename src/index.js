fef863c1432379c88d5368e85d37aad6e1bc4bce
andres.cuervo@webfg.com <andres.cuervo@webfg.com>
2022-03-23 17:43:09 +0100

    change server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://lift-off-catsastronauts-server.herokuapp.com/', // URL production
   cache: new InMemoryCache(),
 });
 
